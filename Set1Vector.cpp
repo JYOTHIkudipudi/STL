c5144c6682d4a3a480261c96ca987d83d9ee8944
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2025-02-06 12:00:16 +0530

    Set1Vector.cpp

--- Set1Vector.cpp
+++ Set1Vector.cpp
@@ -14,54 +14,29 @@ A query can be of five types
 Output:
 The output for each test case will  be space separated integers denoting the results of each query .    */
 
-#include <bits/stdc++.h>  
-using namespace std;
-/**
- * Adds an element to the end of the vector.
- * @param A Reference to the vector.
- * @param x The integer to be added.
- */
+
 void add_to_vector(vector<int> &A, int x) {
     A.push_back(x);
 }
 
-/**
- * Sorts the vector in ascending order.
- * @param A Reference to the vector.
- */
 void sort_vector_asc(vector<int> &A) {
     sort(A.begin(), A.end());
 }
 
-/**
- * Reverses the order of elements in the vector.
- * @param A Reference to the vector.
- */
 void reverse_vector(vector<int> &A) {
     reverse(A.begin(), A.end());
 }
 
-/**
- * Returns the size of the vector.
- * @param A Reference to the vector.
- * @return The number of elements in the vector.
- */
 int size_of_vector(vector<int> &A) {
     return A.size();
 }
 
-/**
- * Sorts the vector in descending order.
- * @param A Reference to the vector.
- */
+
 void sort_vector_desc(vector<int> &A) {
     sort(A.begin(), A.end(), greater<int>());
 }
 
-/**
- * Prints the elements of the vector, space-separated.
- * @param A Reference to the vector.
- */
+
 void print_vector(vector<int> &A) {
     for (auto it = A.begin(); it != A.end(); it++) {
         cout << (*it) << " ";
